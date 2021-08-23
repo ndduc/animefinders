6bb92670e8e64f91ec91f4f7898d3448d2768315
ndduc <ndduc1856@gmail.com>
2021-08-22 18:02:33 -0700

    added manga, light novel components, updated ci

---
+++ src/app/manga/manga.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MangaComponent } from './manga.component';
+
+describe('MangaComponent', () => {
+  let component: MangaComponent;
+  let fixture: ComponentFixture<MangaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MangaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MangaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/manga/manga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manga',
+  templateUrl: './manga.component.html',
+  styleUrls: ['./manga.component.css']
+})
+export class MangaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
