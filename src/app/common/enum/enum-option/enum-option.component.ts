7716cfaa2844acc0a7435002b7c31c56613144ac
ndduc <ndduc1856@gmail.com>
2021-08-23 21:31:41 -0700

    added sort by type and rate

---
+++ src/app/common/enum/enum-option/enum-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enum-option',
+  templateUrl: './enum-option.component.html',
+  styleUrls: ['./enum-option.component.css']
+})
+export class EnumOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
